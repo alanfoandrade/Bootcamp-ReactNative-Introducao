0a13f709e4acfaef676e328534c0d333ba91df4b
alanfoandrade <alanfrank@gec.inatel.br>
2019-11-06 00:22:43 -0300

    Add axios and config github api connection

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,6 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
-    'react/static-property-placement': 'off',
     'react/jsx-props-no-spreading': 'off',
   },
 };
